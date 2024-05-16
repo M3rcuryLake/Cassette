8a86e4368e2a73e64892f35a9e330101b9c0fde9
M3rcuryLake <ankitmukherjee814@gmail.com>
2024-05-17 00:06:20 +0530

    version 1.0.0.0

---
+++ main.py
@@ -0,0 +1,298 @@
+from g4f.client import Client
+import requests
+import assemblyai as aai
+from pydub import AudioSegment
+import random 
+from moviepy.editor import *
+from math import ceil
+from os import system
+import sys
+from unrealspeech import UnrealSpeechAPI, play, save
+import seewav
+import tempfile
+from pathlib import Path
+from time import sleep
+
+
+
+
+assmebly_ai_api= "712da2ae0de14a10b04da29feee10fb8"
+urs_api="EgDQ8NlKEcZhvSDqnewrSqgoKSCXMvWTO1BozKgmBJy15embpNoM80"
+
+def pfp_wave(buzz):
+    if buzz==1:
+        video_p='pictures/Denn.png'
+    if buzz==2:
+        video_p='pictures/Jed.jpeg'        
+    if buzz==3:
+        video_p='pictures/Jess.jpeg'
+    if buzz==4:
+        video_p='pictures/K-11.jpeg'
+    if buzz==5:
+        video_p='pictures/Kee.png'                   
+    if buzz==6:
+        video_p='pictures/Kell.jpeg'
+    if buzz==7:
+        video_p='pictures/Ron.jpeg'
+    
+    #use seewav to generate waveform video
+    #The 'seewav' module in the given codebase is a modded version of a pull request by @Phoenix616 in the github page of the base seewav module
+
+    with tempfile.TemporaryDirectory() as tmp:
+        seewav.visualize(audio=Path('output.mp3'),
+                        tmp=Path(tmp),
+                        out=Path("waves.mp4"),
+                        fg_color=[1, 0.78, 0],
+                        bg_color=[0.21, 0.22, 0.24],
+                        size=(480, 480),
+                        bars=70
+                        )
+    
+    
+    video= VideoFileClip("waves.mp4")
+    video_duration = video.duration
+    x_pos = 20
+    y_pos = 'center' 
+
+    title = (
+        ImageClip(video_p)
+        .set_pos((x_pos, y_pos))
+        .resize(height=150)
+    )
+    final = CompositeVideoClip([video.set_duration(video_duration), title.set_duration(video_duration)])
+    final.write_videofile("wave_out.mp4")
+
+
+def v_merger(file1, file2):
+
+    #split screen two videos
+
+    clip1 = VideoFileClip(file1)
+    clip2 = VideoFileClip(file2)
+ 
+    final = clips_array([[clip1], [clip2]])
+    final.write_videofile("merged.mp4")
+
+
+def a_mixer(num):
+
+    #mixes base audio with a background music
+
+    if num==1: au_path = "Minecraft"
+    if num==2: au_path = "Subwoofer_Lullaby"
+    if num==3: au_path = "Moog_City_2"
+
+    sound1 = AudioSegment.from_file(f"music/{au_path}.mp3", format="mp3")
+    sound2 = AudioSegment.from_file("output.mp3", format="mp3")
+
+    overlay = sound2.overlay(sound1, position=0)
+    overlay.export("F_output.mp3", format="mp3")
+
+def backdrop(buzz):
+
+    #subclips the gameplay video according to the playtime of the audio
+
+    #buzz=QuerList('backdrop')
+    audio_path='output.mp3'
+
+    if buzz==1:
+        video_path='backdrop/minecraft.mp4'
+    if buzz==2:
+        video_path='backdrop/fh5.mp4'        
+    if buzz==3:
+        video_path='backdrop/gtav.mp4'
+    if buzz==4:
+        video_path='backdrop/trackmania.mp4'
+
+
+    def aud_dur(file_path):
+       audio_file = AudioSegment.from_file(file_path)
+       duration = audio_file.duration_seconds
+       return duration
+    
+
+    def vid_dur(file_path):
+       TempClip= VideoFileClip(file_path)
+       vid_duration=TempClip.duration
+       return vid_duration
+    
+
+    audio_duration = aud_dur(audio_path)
+    video_duration = vid_dur(video_path)
+    print(f"Audio Duration: {audio_duration} seconds, Video Duration: {video_duration}")
+    
+    s_time = random.randint(0,ceil(video_duration-(audio_duration + 5)))
+    e_time = s_time+ ceil(audio_duration)
+    print(s_time, e_time)
+    video= VideoFileClip(video_path).subclip(s_time,e_time)
+    video.write_videofile('temp1.mp4')
+
+def sub_append():
+
+    #adds subtitle
+
+    system("ffmpeg -i subs.srt subtitle.ass")   
+    ass_file_path = 'subtitle.ass'
+    new_style_definition = 'Style: Default,Permanent Marker,15 ,&H0099ff,&Hffffff,&H0,&H0,1,0,0,0,100,100,0,0,1,1,2,5,50,50,50,1\n'
+ 
+    with open(ass_file_path, 'r', encoding='utf-8') as file:
+         lines = file.readlines()
+
+    for line in lines:
+        if line.strip().startswith('Style:'):
+            lines[(lines.index(line))]=new_style_definition
+    with open('subtitle.ass', 'w') as file:
+        file.write(''.join(lines))
+    
+    system('''ffmpeg -i temp2.mp4 -vf "ass=subtitle.ass" -c:a copy -c:v libx264 -crf 23 -preset veryfast Final_Output.mp4''')
+
+
+def add_aud(fil, aud):
+    
+    #adds audio to the given video
+    
+    videoclip = VideoFileClip(fil)
+    audioclip = AudioFileClip(aud)
+
+    new_audioclip = CompositeAudioClip([audioclip])
+    videoclip.audio = new_audioclip
+    videoclip.write_videofile("temp2.mp4", codec='libx264', audio_codec='aac')
+
+
+def subs():
+
+    #generates subtitles using AssemblyAI API  
+
+    aai.settings.api_key = assmebly_ai_api
+    transcriber = aai.Transcriber()
+    transcript = transcriber.transcribe('./output.mp3')
+    with open(f'subs.srt', 'a+') as handler:
+        handler.write(transcript.export_subtitles_srt())
+
+def model(buzz):
+
+    #generates Transcript of the video using g4f gpt 3.5 Turbo model
+
+    client = Client()
+    response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": "Just answer the given question under 60 words in English, DO NOT include any titles, bullet points, Citations, or any special charecters, just the transcript. Now here is the Prompt :  " + buzz  }],
+    )
+    gen=response.choices[0].message.content
+    return(gen)
+
+def voice_charecter(charecter, trs):
+
+    #generates voice
+
+    api_key = urs_api
+    speech_api = UnrealSpeechAPI(api_key)
+    text_to_speech = trs
+    timestamp_type = "sentence"  # Choose from 'sentence' or 'word'
+    voice_id = charecter  # Choose the desired voice
+    bitrate = "192k"
+    speed = 0 
+    pitch = 1.0
+    audio_data = speech_api.speech(text=text_to_speech,voice_id=voice_id, bitrate=bitrate, timestamp_type=timestamp_type, speed=speed, pitch=pitch)
+    save(audio_data, "output.mp3")
+
+
+def clear():
+    if sys.platform.startswith('linux'):
+        system("clear")
+    elif sys.platform.startswith('win'):
+        system("cls")
+    print('\033[94m'+'''
+░░      ░░░░      ░░░░      ░░░░      ░░░        ░░        ░░        ░░        ░
+▒  ▒▒▒▒  ▒▒  ▒▒▒▒  ▒▒  ▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒  ▒▒▒▒▒  ▒▒▒▒▒▒▒
+▓  ▓▓▓▓▓▓▓▓  ▓▓▓▓  ▓▓▓      ▓▓▓▓      ▓▓▓      ▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓  ▓▓▓▓▓      ▓▓▓
+█  ████  ██        ████████  ████████  ██  ███████████  ████████  █████  ███████
+██      ███  ████  ███      ████      ███        █████  ████████  █████        █
+                                                                      
+''')
+
+def cleanup():
+    if sys.platform.startswith('linux'): system("rm -rf F_output.mp3 merged.mp4 output.mp3 subs.srt subtitle.ass temp1.mp4 temp2.mp4 wave_out.mp4 waves.mp4")
+    elif sys.platform.startswith('win'): system("del F_output.mp3 merged.mp4 output.mp3 subs.srt subtitle.ass temp1.mp4 temp2.mp4 wave_out.mp4 waves.mp4")
+
+def main():
+    clear()
+    QueryList={}
+    QueryList["script"]=input('\033[94m'+'''1. Transcript Prompt :
+    
+>>>  ''')
+    clear()
+    QueryList["music"]=int(input('''\n2. Use Background music?
+2.1 : Minecraft (press 1)
+2.2 : Subwoofer Lullaby (press 2)
+2.3 : Moog City 2 (press 3)
+
+>>> '''))
+    clear()
+    QueryList["voices"]=input(f'''\n3. Coose a voice :
+3.1 : Dan: Young Male
+3.2 : Will: Mature Male
+3.3 : Scarlett: Young Female
+3.4 : Liv: Young Female
+3.5 : Amy: Mature Female
+
+>>> ''')
+    clear()
+    QueryList["backdrop"]=int(input('''4. Choose a background game.
+4.1 : Minecraft (press 1)
+4.2 : Forza Horizon 5 (press 2)
+4.3 : GTAV (press 3)
+4.4 : trackmania (press 4)
+
+
+>>>'''))
+    clear()
+    QueryList["charecter"]=int(input('''4. Choose a charecter image.
+5.1 : Denn (press 1)
+5.2 : Jed (press 2)
+5.3 : Jess (press 3)
+5.4 : K-11 (press 4)
+5.5 : Kee (press 5)
+5.6 : Kell (press 6)
+5.7 : Ron (press 7)           
+
+>>>'''))
+#    clear()
+    
+    script = model(QueryList['script'])
+    sleep(2)
+    print(script)
+    print('\n'+'Script Generated\n')
+    
+    voice_charecter(QueryList['voices'], script)
+    print('\n'+' Voice Generated\n')
+    sleep(2)
+    subs()
+    
+    backdrop(QueryList["backdrop"])
+    print('\n Backdrop Generated\n')
+   
+    pfp_wave(QueryList['charecter'])
+    sleep(2)
+    a_mixer(QueryList['music'])
+    v_merger('wave_out.mp4', 'temp1.mp4')
+    add_aud("merged.mp4", "F_output.mp3")
+    sleep(2)
+    sub_append()
+    print("\n Job Finished")
+    
+    delr=input("delete all non essential files? (y/n) : ")
+    if delr=="y" : cleanup()
+    if delr=="n" : pass
+    
+if __name__ == "__main__":
+    _is_main = True
+    main()
+    
+    
+
+
+
+    
+       
+
---
+++ seewav/seewav.py
@@ -0,0 +1,317 @@
+#!/usr/bin/env python3
+# This is free and unencumbered software released into the public domain. For more detail,
+# see the LICENCE file at https://github.com/adefossez/seewav
+# Original author: adefossez
+"""
+Generates a nice waveform visualization from an audio file, save it as a mp4 file.
+"""
+import argparse
+import json
+import math
+import subprocess as sp
+import sys
+import tempfile
+from pathlib import Path
+
+import cairo
+import PIL.Image as Image
+import numpy as np
+import tqdm
+
+_is_main = False
+
+
+def colorize(text, color):
+    """
+    Wrap `text` with ANSI `color` code. See
+    https://stackoverflow.com/questions/4842424/list-of-ansi-color-escape-sequences
+    """
+    code = f"\033[{color}m"
+    restore = "\033[0m"
+    return "".join([code, text, restore])
+
+
+def fatal(msg):
+    """
+    Something bad happened. Does nothing if this module is not __main__.
+    Display an error message and abort.
+    """
+    if _is_main:
+        head = "error: "
+        if sys.stderr.isatty():
+            head = colorize("error: ", 1)
+        print(head + str(msg), file=sys.stderr)
+        sys.exit(1)
+
+
+def read_info(media):
+    """
+    Return some info on the media file.
+    """
+    proc = sp.run([
+        'ffprobe', "-loglevel", "panic",
+        str(media), '-print_format', 'json', '-show_format', '-show_streams'
+    ],
+                  capture_output=True)
+    if proc.returncode:
+        raise IOError(f"{media} does not exist or is of a wrong type.")
+    return json.loads(proc.stdout.decode('utf-8'))
+
+
+def read_audio(audio, seek=None, duration=None):
+    """
+    Read the `audio` file, starting at `seek` (or 0) seconds for `duration` (or all)  seconds.
+    Returns `float[channels, samples]`.
+    """
+
+    info = read_info(audio)
+    channels = None
+    stream = info['streams'][0]
+    if stream["codec_type"] != "audio":
+        raise ValueError(f"{audio} should contain only audio.")
+    channels = stream['channels']
+    samplerate = float(stream['sample_rate'])
+
+    # Good old ffmpeg
+    command = ['ffmpeg', '-y']
+    command += ['-loglevel', 'panic']
+    if seek is not None:
+        command += ['-ss', str(seek)]
+    command += ['-i', audio]
+    if duration is not None:
+        command += ['-t', str(duration)]
+    command += ['-f', 'f32le']
+    command += ['-']
+
+    proc = sp.run(command, check=True, capture_output=True)
+    wav = np.frombuffer(proc.stdout, dtype=np.float32)
+    return wav.reshape(-1, channels).T, samplerate
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def envelope(wav, window, stride):
+    """
+    Extract the envelope of the waveform `wav` (float[samples]), using average pooling
+    with `window` samples and the given `stride`.
+    """
+    # pos = np.pad(np.maximum(wav, 0), window // 2)
+    wav = np.pad(wav, window // 2)
+    out = []
+    for off in range(0, len(wav) - window, stride):
+        frame = wav[off:off + window]
+        out.append(np.maximum(frame, 0).mean())
+    out = np.array(out)
+    # Some form of audio compressor based on the sigmoid.
+    out = 1.9 * (sigmoid(2.5 * out) - 0.5)
+    return out
+    
+def pil_to_surface(image):
+    """
+    Internal function, create cairo surface from Pillow image
+    """
+    if 'A' not in image.getbands():
+        image.putalpha(int(256))
+    return cairo.ImageSurface.create_for_data(bytearray(image.tobytes('raw', 'BGRa')), cairo.FORMAT_ARGB32, image.width, image.height)
+
+def draw_env(envs, out, fg_colors, fg_opacity, bg_color, bg_image, center, size):
+    """
+    Internal function, draw a single frame (two frames for stereo) using cairo and save
+    it to the `out` file as png. envs is a list of envelopes over channels, each env
+    is a float[bars] representing the height of the envelope to draw. Each entry will
+    be represented by a bar.
+    """
+    if bg_image is None:
+        surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, *size)
+        offset = [0, 0]
+    else:
+        surface = pil_to_surface(bg_image)
+        # offset needs to be relative to the size of the surface, not the size of the background image
+        offset = [
+            (bg_image.width * center[0] - size[0] / 2) / size[0],
+            (bg_image.height * center[1] - size[1] / 2) / size[1]
+        ]
+    ctx = cairo.Context(surface)
+    ctx.scale(*size)
+    if bg_image is None:
+        ctx.set_source_rgb(*bg_color)
+        ctx.rectangle(0, 0, 1, 1)
+        ctx.fill()
+
+    K = len(envs) # Number of waves to draw (waves are stacked vertically)
+    T = len(envs[0]) # Numbert of time steps
+    pad_ratio = 0.1 # spacing ratio between 2 bars
+    width = 1. / (T * (1 + 2 * pad_ratio))
+    pad = pad_ratio * width
+    delta = 2 * pad + width
+
+    ctx.translate(*offset)
+    ctx.set_line_width(width)
+    for step in range(T):
+        for i in range(K):
+            half = 0.5 * envs[i][step] # (semi-)height of the bar
+            half /= K # as we stack K waves vertically
+            midrule = (1+2*i)/(2*K) # midrule of i-th wave
+            ctx.set_source_rgba(*fg_colors[i], fg_opacity)
+            ctx.move_to(pad + step * delta, midrule - half)
+            ctx.line_to(pad + step * delta, midrule)
+            ctx.stroke()
+            ctx.set_source_rgba(*fg_colors[i], fg_opacity - fg_opacity / 5)
+            ctx.move_to(pad + step * delta, midrule)
+            ctx.line_to(pad + step * delta, midrule + 0.9 * half)
+            ctx.stroke()
+
+    surface.write_to_png(out)
+
+
+def interpole(x1, y1, x2, y2, x):
+    return y1 + (y2 - y1) * (x - x1) / (x2 - x1)
+
+
+def visualize(audio,
+              tmp,
+              out,
+              seek=None,
+              duration=None,
+              rate=60,
+              bars=50,
+              speed=4,
+              time=0.4,
+              oversample=3,
+              fg_color=(.2, .2, .2),
+              fg_color2=(.5, .3, .6),
+              fg_opacity=1,
+              bg_color=(1, 1, 1),
+              bg_image=None,
+              center=(.5, .5),
+              size=(400, 300),
+              stereo=False,
+              ):
+    """
+    Generate the visualisation for the `audio` file, using a `tmp` folder and saving the final
+    video in `out`.
+    `seek` and `durations` gives the extract location if any.
+    `rate` is the framerate of the output video.
+
+    `bars` is the number of bars in the animation.
+    `speed` is the base speed of transition. Depending on volume, actual speed will vary
+        between 0.5 and 2 times it.
+    `time` amount of audio shown at once on a frame.
+    `oversample` higher values will lead to more frequent changes.
+    `fg_color` is the rgb color to use for the foreground.
+    `fg_color2` is the rgb color to use for the second wav if stereo is set.
+    `bg_color` is the rgb color to use for the background.
+    `bg_image` is the path to the PNG image to use for the background.
+    `size` is the `(width, height)` in pixels to generate.
+    `stereo` is whether to create 2 waves.
+    """
+    try:
+        wav, sr = read_audio(audio, seek=seek, duration=duration)
+    except (IOError, ValueError) as err:
+        fatal(err)
+        raise
+
+    output_size = size
+    image = None
+    if bg_image is not None:
+        try:
+            image = Image.open(bg_image)
+        except (IOError, ValueError) as err:
+            fatal(err)
+            raise
+        # resize image to be compatible with ffmpeg
+        if image.width % 2 == 1:
+            image = image.resize((image.width + 1, image.height))
+        if image.height % 2 == 1:
+            image = image.resize((image.width, image.height + 1))
+        output_size = image.width, image.height
+
+    # wavs is a list of wav over channels
+    wavs = []
+    if stereo:
+        assert wav.shape[0] == 2, 'stereo requires stereo audio file'
+        wavs.append(wav[0])
+        wavs.append(wav[1])
+    else:
+        wav = wav.mean(0)
+        wavs.append(wav)
+
+    for i, wav in enumerate(wavs):
+        wavs[i] = wav/wav.std()
+
+    window = int(sr * time / bars)
+    stride = int(window / oversample)
+    # envs is a list of env over channels
+    envs = []
+    for wav in wavs:
+        env = envelope(wav, window, stride)
+        env = np.pad(env, (bars // 2, 2 * bars))
+        envs.append(env)
+
+    duration = len(wavs[0]) / sr
+    frames = int(rate * duration)
+    smooth = np.hanning(bars)
+
+    print("Generating the frames...")
+    for idx in tqdm.tqdm(range(frames), unit=" frames", ncols=80):
+        pos = (((idx / rate)) * sr) / stride / bars
+        off = int(pos)
+        loc = pos - off
+        denvs = []
+        for env in envs:
+            env1 = env[off * bars:(off + 1) * bars]
+            env2 = env[(off + 1) * bars:(off + 2) * bars]
+
+            # we want loud parts to be updated faster
+            maxvol = math.log10(1e-4 + env2.max()) * 10
+            speedup = np.clip(interpole(-6, 0.5, 0, 2, maxvol), 0.5, 2)
+            w = sigmoid(speed * speedup * (loc - 0.5))
+            denv = (1 - w) * env1 + w * env2
+            denv *= smooth
+            denvs.append(denv)
+        draw_env(denvs, tmp / f"{idx:06d}.png", (fg_color, fg_color2), fg_opacity, bg_color, image, center, size)
+
+    audio_cmd = []
+    if seek is not None:
+        audio_cmd += ["-ss", str(seek)]
+    audio_cmd += ["-i", audio.resolve()]
+    if duration is not None:
+        audio_cmd += ["-t", str(duration)]
+    print("Encoding the animation video... ")
+    # https://hamelot.io/visualization/using-ffmpeg-to-convert-a-set-of-images-into-a-video/
+    sp.run([
+        "ffmpeg", "-y", "-loglevel", "panic", "-r",
+        str(rate), "-f", "image2", "-s", f"{output_size[0]}x{output_size[1]}", "-i", "%06d.png"
+    ] + audio_cmd + [
+        "-c:a", "aac", "-vcodec", "libx264", "-crf", "10", "-pix_fmt", "yuv420p", "-shortest",
+        out.resolve()
+    ],
+           check=True,
+           cwd=tmp)
+
+
+def parse_color(colorstr):
+    """
+    Given a comma separated rgb(a) colors, returns a 4-tuple of float.
+    """
+    try:
+        r, g, b = [float(i) for i in colorstr.split(",")]
+        return r, g, b
+    except ValueError:
+        fatal("Format for color is 3 floats separated by commas 0.xx,0.xx,0.xx, rgb order")
+        raise
+
+def parse_coords(coordsstr):
+    """
+    Given a comma separated float x and y coords, returns a tuple of float.
+    """
+    try:
+        x, y = [float(i) for i in coordsstr.split(",")]
+        return x, y
+    except ValueError:
+        fatal("Format for coords is 2 floats separated by commas 0.x,0.y, xy order")
+        raise
+
+
